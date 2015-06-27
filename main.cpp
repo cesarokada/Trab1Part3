ef98f0e3648938322e1b4459b905e9e736fe54e4
toquete <toquete@rc.unesp.br>
2015-06-27 16:39:21 -0300

    Padronização do código e translação

--- main.cpp
+++ main.cpp
@@ -2,146 +2,140 @@
 #include<stdio.h>
 #include<math.h>
 
-int a,b,c,d,e,f,g,h,tx,ty;
-float angulo;
+#define NUM_PONTOS 4
 
-void init(void)
+typedef struct tPontos{
+  int x;
+  int y;
+} Pontos;
+
+Pontos points[NUM_PONTOS];
+
+void inicializaOpenGL()
 {
- glClearColor(0.0,0.0,0.0,1.0);
- glMatrixMode(GL_PROJECTION);
- gluOrtho2D(0.0,200.0,0.0,200.0);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //Define o modo inicial de apresentação
+  glutInitWindowPosition(50,50); // Define a posição inicial da tela a ser apresentada
+  glutInitWindowSize(500, 500); // Define o tamanho da tela a ser apresentada
 }
 
-void setpoint(GLint x,GLint y)
+void inicializaCores()
 {
-    glBegin(GL_POINTS);
-    glVertex2i(x,y);
-    glEnd();
-}
-
-void transform(int x,int y,int tx,int ty)
-{
-    glColor3f (0.0, 1.0, 0.0);
-    setpoint(x+tx,y+ty);
-    glColor3f (255.0, 255.0, 255.0);
-}
-
-void rotate(int x,int y,float angulo){
-    glColor3f (0.0, 1.0, 0.0);
-    glRotatef(angulo,0.0,0.0,1.0);
-    setpoint(x,y);
-    glColor3f (255.0, 255.0, 255.0);
+  glClearColor(1.0,1.0,1.0,0);// define a cor de fundo da janela (no caso, branco)
+  glColor3f(1.0,0.0,0.0);// define cor do traço (no caso, vermelho)
+  glPointSize(1.0);// define espessura do traço
+  gluOrtho2D(0 , 500 , 0 , 500);
 }
 
 int round1(double number)
 {
- return (number>=0) ? (int)(number+0.5):(int)(number-0.5);
+  return (number>=0) ? (int)(number+0.5):(int)(number-0.5);
 }
-void LineWithDDA(int x0,int y0,int x1,int y1)
+
+void DDA(int x0,int y0,int x1,int y1)
 {
- int dy=y1-y0;
- int dx=x1-x0;
- int steps,i;
- float xinc,yinc,x=x0,y=y0;
- if(abs(dx)>abs(dy))
- {
-  steps=abs(dx);
- }
- else
- {
-  steps=abs(dy);
- }
- xinc=(float)dx/(float)steps;
- yinc=(float)dy/(float)steps;
- setpoint(round1(x),round1(y));
- transform(round1(x),round1(y),tx,ty);
- //rotate(round1(x),round1(y),angulo);
- for(i=0;i<steps;i++)
- {
-  x+=xinc;
-  y+=yinc;
-  setpoint(round1(x),round1(y));
-  transform(round1(x),round1(y),tx,ty);
-  //rotate(round1(x),round1(y),angulo);
- }
- glutSwapBuffers();
+  int dy = y1 - y0;
+  int dx = x1 - x0;
+  int passos, i;
+  float xinc, yinc, x = x0, y = y0;
+
+  if(abs(dx) > abs(dy))
+    passos = abs(dx);
+  else
+    passos = abs(dy);
+
+  xinc = (float)dx / (float)passos;
+  yinc = (float)dy / (float)passos;
+
+  glVertex2i(round1(x), round1(y));
+  for(i=0; i < passos; i++)
+  {
+    x += xinc;
+    y += yinc;
+    glVertex2i(round1(x), round1(y));
+  }
 }
 
 void draw()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    LineWithDDA(a,b,c,d);
-    LineWithDDA(c,d,e,f);
-    LineWithDDA(e,f,g,h);
-    LineWithDDA(g,h,a,b);
+    glBegin(GL_POINTS);
+    for(int i = 0; i < NUM_PONTOS; i++)
+    {
+      if((i + 1) == NUM_PONTOS)
+        DDA(points[i].x, points[i].y,
+            points[0].x, points[0].y);
+      else
+        DDA(points[i].x, points[i].y,
+            points[i + 1].x, points[i + 1].y);
+    }
+    glEnd();
     glFlush();
 }
 
-void validaOpcao(int opcao){
-    system("cls");
-    switch(opcao){
-        case 1:
-            printf("\nTranslacao em x: ");
-            scanf("%d",&tx);
-            printf("\nTranslacao em y: ");
-            scanf("%d",&ty);
-            glutCreateWindow("Translacao Poligono");
-            init();
-            glutDisplayFunc(draw);
-            glutMainLoop();
-            break;
-        case 2:
-            printf("\nValor do angulo: ");
-            scanf("%f",&angulo);
-            glutCreateWindow("Rotacao Poligono");
-            init();
-            glutDisplayFunc(draw);
-            glutMainLoop();
-            break;
-        case 0:
-            break;
-    }
+void translacao()
+{
+  int tx, ty;
+
+  printf("\nTranslacao em X: ");
+  scanf("%d",&tx);
+  printf("\nTranslacao em Y: ");
+  scanf("%d",&ty);
+
+  for(int i = 0; i < NUM_PONTOS; i++)
+  {
+    points[i].x += tx;
+    points[i].y += ty;
+  }
+  inicializaOpenGL();
+  glutCreateWindow("Translacao Poligono");
+  inicializaCores();
+  glutDisplayFunc(draw);
+  glutMainLoop();
 }
 
-void showMenu(){
-
-    int op;
-    system("cls");
-    printf("--------------MENU--------------\n");
-    printf("1 - Translacao\n");
-    printf("2 - Rotacao\n");
-    printf("0 - Sair\n");
-    printf("Opcao: ");
-    scanf("%d",&op);
-    validaOpcao(op);
+void validaOpcao(int opcao)
+{
+  system("cls");
+  switch(opcao){
+    case 1:
+      translacao();
+      break;
+    case 2:
+      break;
+    case 0:
+      break;
+  }
 }
 
+void showMenu()
+{
+  int op;
+
+  system("cls");
+  printf("--------------MENU--------------\n");
+  printf("1 - Translacao\n");
+  printf("2 - Rotacao\n");
+  printf("0 - Sair\n");
+  printf("Opcao: ");
+  scanf("%d",&op);
+  validaOpcao(op);
+}
+
+void definePontos()
+{
+  printf("VERTICES DO POLIGONO\n\n");
+  for(int i = 0; i < NUM_PONTOS; i++)
+  {
+    printf("X%d: ", i + 1);
+    scanf("%d", &points[i].x);
+    printf("Y%d: ", i + 1);
+    scanf("%d", &points[i].y);
+  }
+}
 int main(int argc,char **argv)
 {
-
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowPosition(50,50);
-    glutInitWindowSize(500,500);
-
-     printf("Vértices do Poligono\n\n");
-     printf("x1 : ");
-     scanf("%d",&a);
-     printf("y1 : ");
-     scanf("%d",&b);
-     printf("x2 : ");
-     scanf("%d",&c);
-     printf("y2 : ");
-     scanf("%d",&d);
-     printf("x3 : ");
-     scanf("%d",&e);
-     printf("y3 : ");
-     scanf("%d",&f);
-     printf("x4 : ");
-     scanf("%d",&g);
-     printf("y4 : ");
-     scanf("%d",&h);
-     showMenu();
-
-     return 0;
+  glutInit(&argc,argv);
+  definePontos();
+  showMenu();
+  return 0;
 }
